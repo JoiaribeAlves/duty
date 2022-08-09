8916f6bb7ae13e8ded662e123f77cdc91e73b547
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-08-09 00:31:33 -0400

    Cria as rotas da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,22 @@
+import "dotenv/config";
 import axios from "axios";
 
 const app = axios.create({
-	baseURL: "http://localhost:4000"
+	baseURL: process.env.API_BASE_URL,
 });
 
-export function getContent() {
-	return app.get("/index");
+export function getFarmacy(id: string) {
+	return app.get(`/pharmacy/${id}`);
+}
+
+export function getFarmacies() {
+	return app.get("/pharmacies");
+}
+
+export function getDuties() {
+	return app.get("/duties");
+}
+
+export function getDuty(date: string) {
+	return app.get(`/duty/${date}`);
 }
