74ee4b9528494bbec40940c62e9fe0979b409650
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-09-04 16:57:53 -0400

    Adiciona o react-query

--- src/components/admin/pharmacies/index.tsx
+++ src/components/admin/pharmacies/index.tsx
@@ -1,21 +1,50 @@
-import { useEffect, useState } from "react";
 import { FaEdit, FaPlus, FaTrashAlt } from "react-icons/fa";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 
 import { LayoutAdm } from "../Layout";
+import { Spiner } from "../../Utils";
 import { deletePharmacy, getPharmacies } from "../../../services/api";
-import { IPharmacy } from "../../../interfaces/index";
 
 import styles from "./Styles.module.scss";
 
 export function PharmaciesAdm() {
-	const [pharmacies, setPharmacies] = useState<IPharmacy[] | []>([]);
+	const tenSecondsInMilliseconds = 1000 * 10;
+	const { data, isLoading, isError } = useQuery(
+		["pharmacies"],
+		() => getPharmacies(),
+		{ staleTime: tenSecondsInMilliseconds }
+	);
 
-	async function loadContent() {
-		const { data } = await getPharmacies();
+	if (isLoading) {
+		return (
+			<LayoutAdm>
+				<Spiner />
+			</LayoutAdm>
+		);
+	}
 
-		setPharmacies(data);
+	if (isError) {
+		toast.error(
+			"Não foi possível obter a lista de farmácias. Tente novamente."
+		);
+
+		return (
+			<LayoutAdm>
+				<>
+					<ToastContainer theme="dark" />
+
+					<button
+						type="button"
+						className={styles.btnRefreshWindow}
+						onClick={() => window.location.reload()}
+					>
+						Tentar novamente
+					</button>
+				</>
+			</LayoutAdm>
+		);
 	}
 
 	async function delPharmacy(id: string) {
@@ -24,16 +53,11 @@ export function PharmaciesAdm() {
 				await deletePharmacy(id);
 
 				toast.success("Farmácia deletada com sucesso.");
-				loadContent();
 			}
 		} catch (error) {
 			toast.error("Falha ao deletar a farmácia.");
 		}
 	}
-
-	useEffect(() => {
-		loadContent();
-	}, []);
 
 	return (
 		<LayoutAdm>
@@ -56,39 +80,31 @@ export function PharmaciesAdm() {
 						</tr>
 					</thead>
 
-					{pharmacies.length > 0 ? (
-						<tbody>
-							{pharmacies.map((pharmacy, index) => (
-								<tr key={index}>
-									<td className={styles.textCenter}>{index + 1}</td>
+					<tbody>
+						{data?.data.map((pharmacy, index) => (
+							<tr key={index}>
+								<td className={styles.textCenter}>{index + 1}</td>
 
-									<td>{pharmacy.name}</td>
+								<td>{pharmacy.name}</td>
 
-									<td className={styles.textCenter}>
-										<Link to={`/admin/farmacia/editar/${pharmacy.id}`}>
-											<FaEdit />
-										</Link>
-									</td>
+								<td className={styles.textCenter}>
+									<Link to={`/admin/farmacia/editar/${pharmacy.id}`}>
+										<FaEdit />
+									</Link>
+								</td>
 
-									<td className={styles.textCenter}>
-										<button
-											type="button"
-											className={styles.btnDelete}
-											onClick={() => delPharmacy(pharmacy.id)}
-										>
-											<FaTrashAlt />
-										</button>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					) : (
-						<tbody>
-							<tr>
-								<td colSpan={4}>Nenhuma farmácia encontrada.</td>
+								<td className={styles.textCenter}>
+									<button
+										type="button"
+										className={styles.btnDelete}
+										onClick={() => delPharmacy(pharmacy.id!)}
+									>
+										<FaTrashAlt />
+									</button>
+								</td>
 							</tr>
-						</tbody>
-					)}
+						))}
+					</tbody>
 				</table>
 			</>
 		</LayoutAdm>
