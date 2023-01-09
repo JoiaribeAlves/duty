58f9d585b94da727cad2d3eabdb906d2910b6c7c
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2023-01-08 22:48:14 -0400

    Modifica o layout dos cards e faz a busca somente dos plantões para o mês corrente.

--- src/components/shifts/index.tsx
+++ src/components/shifts/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import format from "date-fns/format";
 import { Helmet } from "react-helmet";
+import { FaCamera, FaInfoCircle } from "react-icons/fa";
 
 import { Layout } from "../Layout";
 import { ErrorLoadData, Spiner } from "../Utils";
@@ -9,9 +10,44 @@ import { getDuties } from "../../services/api";
 import styles from "./Styles.module.scss";
 
 export function Shifts() {
-	const { data, isError, isLoading } = useQuery(["shifts"], () => getDuties(), {
-		staleTime: 1000 * 60, // One minute
-	});
+	function getCurrentMonth() {
+		const month = new Date().getMonth();
+
+		switch (month) {
+			case 0:
+				return "january";
+			case 1:
+				return "february";
+			case 2:
+				return "march";
+			case 3:
+				return "april";
+			case 4:
+				return "may";
+			case 5:
+				return "june";
+			case 6:
+				return "july";
+			case 7:
+				return "august";
+			case 8:
+				return "september";
+			case 9:
+				return "october";
+			case 10:
+				return "november";
+			default:
+				return "december";
+		}
+	}
+
+	const { data, isError, isLoading } = useQuery(
+		["shifts"],
+		() => getDuties(getCurrentMonth()),
+		{
+			staleTime: 1000 * 60, // One minute
+		}
+	);
 
 	if (isLoading) {
 		return <Spiner />;
@@ -44,18 +80,30 @@ export function Shifts() {
 			<Helmet>
 				<title>Escala de Plantões</title>
 
-				<meta name="description" content="Escala de plantões de farmácia durante o mês corrente." />
+				<meta
+					name="description"
+					content={`Escala de plantões de farmácia durante o mês de ${currentMonth}`}
+				/>
 			</Helmet>
 
 			<Layout>
 				<h1 className={styles.title}>
-					Lista de plantões para o mês de {currentMonth}
+					Escala de plantões para o mês de {currentMonth}
 				</h1>
 
 				<ul className={styles.shifts}>
 					{data?.data.map((shift) => (
 						<li key={shift.id}>
-							<p>{format(new Date(shift.startDate), "dd/MM")}</p>
+							<span>
+								<FaCamera />
+							</span>
+
+							<p>{format(new Date(shift.startDate), "dd - MM - yy")}</p>
+
+							<a href={`/plantoes/${shift.pharmacyId}`}>
+								<FaInfoCircle />
+								Detalhes
+							</a>
 						</li>
 					))}
 				</ul>
