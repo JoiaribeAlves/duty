16c0b89c3769fd3df16b1ea8654d8ca5278b8a64
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2023-02-01 00:42:03 -0400

    Adiciona a rota /termos-de-uso

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Main } from "./components/main";
 import { Shifts } from "./components/shifts";
 import { About } from "./components/about";
+import { Terms } from "./components/terms";
 import { NotFound } from "./components/not-found";
 
 export function MainRoutes() {
@@ -12,6 +13,7 @@ export function MainRoutes() {
 				<Route path="/" element={<Main />} />
 				<Route path="/plantoes" element={<Shifts />} />
 				<Route path="/sobre" element={<About />} />
+				<Route path="/termos-de-uso" element={<Terms />} />
 
 				<Route path="*" element={<NotFound />} />
 			</Routes>
