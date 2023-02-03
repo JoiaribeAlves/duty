372bd80d9d6a02de69e8b4d0d01c14529c157b96
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2023-02-03 19:28:07 -0400

    Renomeia o componente Spiner para Spinner

--- src/components/Utils.tsx
+++ src/components/Utils.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 
-export function Spiner() {
+export function Spinner() {
 	return (
-		<div className="spiner">
+		<div className="spinner">
 			<span></span>
 		</div>
 	);
